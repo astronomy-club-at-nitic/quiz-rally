5a8f356928023c51d408f6c5bdb7c9303eeb38ee
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2023-08-23 20:43:39 +0900

    refactor: ♻️ (constant) 型`StaticImageData`をパッケージ`next/image`から読み込むようにした。(#62)

--- src/type/static-image-data.ts
+++ src/type/static-image-data.ts
@@ -1,8 +1,1 @@
-export type StaticImageData = {
-  src: string;
-  height: number;
-  width: number;
-  blurDataURL?: string;
-  blurWidth?: number;
-  blurHeight?: number;
-};
+export type { StaticImageData } from 'next/image';
