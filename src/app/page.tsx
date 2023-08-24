40a788bc67cdc7d3d68c342dc4d75015a8e01ad6
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2023-08-25 03:38:02 +0900

    wip: 🚧 (`/`) 質問カードの仮の表示を追加した。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import type { ReactNode } from 'react';
 import JumbotronBackgroundImage from '@/asset/jumbotron/background.png';
 import { Image } from '@/component/common/image';
-// import { QuestionCardLink } from '@/component/questions/question-card-link';
+import { Link } from '@/component/common/link';
+import { QuestionCard } from '@/component/questions/question-card';
 import { questions } from '@/constant/question';
 import { BrandIcon } from '@/icon/brand-icon';
 import { breakpoints } from '@/style/token';
@@ -20,11 +21,21 @@ const RootPage = (): ReactNode => (
       <BrandIcon className="absolute left-1/2 top-1/2 h-[120%] max-w-[84%] -translate-x-1/2 -translate-y-1/2" />
     </div>
     <div className="grid grid-cols-[repeat(auto-fit,288px)] justify-center gap-6 py-12 tablet:grid-cols-[repeat(auto-fit,384px)]">
-      {questions.map(
-        (/* question */) =>
-          // <QuestionCardLink key={question.id} question={question} />
-          null,
-      )}
+      {questions.map((question) => (
+        <Link key={question.id} href={`/${question.id}`}>
+          <QuestionCard {...question} status="correct" />
+        </Link>
+      ))}
+      {questions.map((question) => (
+        <Link key={question.id} href={`/${question.id}`}>
+          <QuestionCard {...question} status="incorrect" />
+        </Link>
+      ))}
+      {questions.map((question) => (
+        <Link key={question.id} href={`/${question.id}`}>
+          <QuestionCard {...question} status="unanswered" />
+        </Link>
+      ))}
     </div>
   </div>
 );
