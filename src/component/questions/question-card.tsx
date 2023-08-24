fa4995ad9063505cc637aef16e02508cca6d364d
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2023-08-25 04:02:51 +0900

    style: 💄 (`<QuestionCard>`) 影を追加した。

--- src/component/questions/question-card.tsx
+++ src/component/questions/question-card.tsx
@@ -10,7 +10,7 @@ type CardProps = ComponentPropsWithoutRef<'div'> &
   Pick<AnswerStatusIconProps, 'status'>;
 
 export const QuestionCard: FC<CardProps> = ({ id, title, thumbnailAlt, thumbnail, status, className, ...props }) => (
-  <div className={cn('flex flex-col items-center overflow-hidden rounded-xl bg-slate-3', className)} {...props}>
+  <div className={cn('flex flex-col items-center overflow-hidden rounded-xl bg-slate-3 shadow-xl', className)} {...props}>
     <Image {...thumbnail} alt={thumbnailAlt} className="h-64 w-full object-cover" />
     <div className="relative flex flex-col items-stretch justify-start gap-2 px-8 py-6">
       <AnswerStatusIcon status={status} className="absolute -top-16 right-2 z-10 rotate-12" />
